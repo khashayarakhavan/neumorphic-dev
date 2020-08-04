ebe08c2c47a3d67942ab325d86f087873b1c3d3f
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-08-04 08:48:18 -0800

    Add loading feature to shop category pages

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
 import CollectionItem from "../../components/atomic/collection-item/collection-item.component";
-import { selectCollection } from "../../redux/shop/shop.selectors";
+import {
+  selectCollection,
+  selectIsCollectionFetching
+} from "../../redux/shop/shop.selectors";
 import {
   CollectionItemsContainer, CollectionPageContainer,
   CollectionTitle
 } from "./collection.styles";
 
-export const CollectionPage = ({ collection }) => {
+export const CollectionPage = ({ collection, isFetching }) => {
   // items is an array so we can `.map` over it.
   // big arrays are better to be changed to hash tables or JS objects for performance optimization.
+  const title = "hello";
+  const items = [];
+  if (isFetching) {
   const { title, items } = collection;
+  console.log('i am fetching');
+  } else {
+    console.log("i am not fetching");
+  }
+  
   return (
+    <div>
+    {!isFetching ? <p>loading content for you</p> : 
     <CollectionPageContainer>
       <CollectionTitle>{title}</CollectionTitle>
       <CollectionItemsContainer>
@@ -20,6 +33,8 @@ export const CollectionPage = ({ collection }) => {
         ))}
       </CollectionItemsContainer>
     </CollectionPageContainer>
+    }
+    </div>
   );
 };
 
@@ -27,6 +42,7 @@ const mapStateToProps = (state, ownProps) => ({
   // Pay attention:
   // here we can also change this to an object in the DB and then change it to array using `object.key()` method.
   collection: selectCollection(ownProps.match.params.collectionId)(state),
+  isFetching: selectCollection(selectIsCollectionFetching)(state),
 });
 
 export default connect(mapStateToProps)(CollectionPage);
